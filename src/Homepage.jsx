53aca200aad33360fe7347147539832e71dcd679
s1r1luxx <155615882+s1r1luxx@users.noreply.github.com>
2024-10-27 09:34:28 +0000

    edit first page + try route

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,2 +1,7 @@
 import React from "react";
-import './Mongkol.css';
+
+export default function Homepage(){
+  return(
+    <h1>This is Homepage</h1>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Monkol from './Monkol';  // การนำเข้าที่แก้ไขสำหรับ Monkol
+import Mongkol from './Mongkol';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import Homepage from './Homepage';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Monkol />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Mongkol />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
