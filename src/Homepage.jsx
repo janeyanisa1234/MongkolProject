129cfdd57e2205b5549ea6e8c18bb63d5e04631d
janeyanisa1234 <129841440+janeyanisa1234@users.noreply.github.com>
2024-10-29 14:01:02 +0000

    page2

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import './Mongkol.css';
-
+import { Routes, Route, Link } from "react-router-dom";
+import Monday from './Monday';
 
 export default function Homepage() {
   return (
     <>
-        <h1 className="logopage">
-          M<img src="/Pictures/sparkling.png" alt="Sparkling" width="20px" height="20" />ngkol
-        </h1>
+        <h1 className="logopage">M<img src="/Pictures/sparkling.png" alt="Sparkling" width="20px" height="20" />ngkol</h1>
+        <Link to="/Monday"><button className="buttonMon">วันจันทร์ </button></Link>
+        <button className="buttonTue">วันอังคาร </button>
+        <button className="buttonWed"> วันพุธ  </button>
+        <button className="buttonThu">วันพฤหัสบดี </button>
+        <button className="buttonFri">วันศุกร์ </button>
+        <button className="buttonSat"> วันเสาร์  </button>
+        <button className="buttonSun"> วันอาทิตย์  </button>
         <div className="flower-bottom-left"></div>
         <div className="flower-top-right"></div>
     </>
   );
+  
 }
---
+++ src/Monday.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Mongkol.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+export default function Monday() {
+  return (
+    <>
+    <div className="container">
+      <h1>สวัสดีวันจันทร์</h1>
+    </div>
+    </>
+  );
+}
